fd4a606a9015cb831f95975745c47d2c4bd73e47
Tuko <tuko.ustx@gmail.com>
2023-08-13 23:10:51 -0500

    Generate self signed cert automatically

---
+++ Samples/cert-self-signed-auto/Program.cs
@@ -0,0 +1,95 @@
+﻿using Opc.Ua;
+using Opc.Ua.Client;
+using Opc.Ua.Configuration;
+using static System.Net.Mime.MediaTypeNames;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        const string appName = "OPC UA minimal client self signed auto";
+        const ApplicationType appType = ApplicationType.Client;
+
+        CertificateIdentifier certificateIdentifier = new CertificateIdentifier();
+        certificateIdentifier.StoreType = "Directory";
+        certificateIdentifier.StorePath = "%LocalApplicationData%/Tuko/pki/own";
+        certificateIdentifier.SubjectName = Utils.ReplaceDCLocalhost("CN=UA Client self signed auto, C=US, S=Texas, O=Tuko, DC=localhost");
+
+        SecurityConfiguration securityConfiguration = new SecurityConfiguration();
+        securityConfiguration.ApplicationCertificate = certificateIdentifier;
+
+        var config = new ApplicationConfiguration()
+        {
+            ApplicationName = appName,
+            ApplicationType = appType,
+            ApplicationUri = Utils.ReplaceLocalhost("urn:localhost:UA:Samples:Self:Sgined:Auto"),
+            ClientConfiguration = new ClientConfiguration()
+            {
+                DefaultSessionTimeout = 60_000,
+            },
+            SecurityConfiguration = securityConfiguration,
+        };
+
+        CertificatePasswordProvider PasswordProvider = new CertificatePasswordProvider(null);
+        ApplicationInstance appInst = new ApplicationInstance
+        {
+            ApplicationName = appName,
+            ApplicationType = appType,
+            CertificatePasswordProvider = PasswordProvider,
+        };
+        appInst.ApplicationConfiguration = config;
+        // check the application certificate.
+        bool haveAppCertificate = await appInst.CheckApplicationInstanceCertificate(false, minimumKeySize: 0).ConfigureAwait(false);
+        if (!haveAppCertificate)
+        {
+            throw new Exception("Application instance certificate invalid!");
+        }
+
+
+        Uri serverUrl = new Uri("opc.tcp://localhost:62541/Quickstarts/ReferenceServer");
+        var endpointDescription = CoreClientUtils.SelectEndpoint(config, serverUrl.ToString(), false);
+
+        EndpointConfiguration endpointConfiguration = EndpointConfiguration.Create(config);
+        ConfiguredEndpoint endpoint = new ConfiguredEndpoint(null, endpointDescription, endpointConfiguration);
+
+        var session = await Opc.Ua.Client.Session.Create(
+            config,
+            null,
+            endpoint,
+            true,
+            false,
+            config.ApplicationName,
+            (uint)config.ClientConfiguration.DefaultSessionTimeout,
+            new UserIdentity(),
+            null
+            ).ConfigureAwait(false);
+
+        // build a list of nodes to be read
+        ReadValueIdCollection nodesToRead = new ReadValueIdCollection()
+                {
+                    // Value of ServerStatus
+                    new ReadValueId() { NodeId = Variables.Server_ServerStatus, AttributeId = Attributes.Value },
+                    // BrowseName of ServerStatus_StartTime
+                    new ReadValueId() { NodeId = Variables.Server_ServerStatus_StartTime, AttributeId = Attributes.BrowseName },
+                    // Value of ServerStatus_StartTime
+                    new ReadValueId() { NodeId = Variables.Server_ServerStatus_StartTime, AttributeId = Attributes.Value }
+                };
+
+        session.Read(
+            null,
+            0,
+            TimestampsToReturn.Both,
+            nodesToRead,
+            out DataValueCollection resultsValues,
+            out DiagnosticInfoCollection diagnosticInfos);
+
+        ClientBase.ValidateResponse(resultsValues, nodesToRead);
+        foreach (DataValue result in resultsValues)
+        {
+            Console.WriteLine("Read Value = {0} , StatusCode = {1}", result.Value, result.StatusCode);
+        }
+        session.Close();
+
+        Console.WriteLine("End OPC UA client");
+    }
+}
